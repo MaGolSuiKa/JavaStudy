22056ccfd57629a822c0e395897f8c19ee19ff79
Magol <source1990820@163.com>
2023-07-03 23:34:57 +0800

    0703 作业（追加文件及路径的保存和图片复制） 提交

--- LearnIO/studyIO/src/com/geekaca/d09/TestFileSearch.java
+++ LearnIO/studyIO/src/com/geekaca/d09/TestFileSearch.java
@@ -1,6 +1,8 @@
 package com.geekaca.d09;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class TestFileSearch {
     public static void main(String[] args) {
@@ -10,8 +12,21 @@ public class TestFileSearch {
          * 根据文件名在目录下递归的搜索
          * 遇到文件就判断名字
          * 遇到文件夹，就递归搜索
+         *
+         * 作业：0703
+         *
+         * 1, 在之前文件搜索的基础上
+         *
+         * 搜索某个目录下的所有.java(.mp3  .mp4 .pdf)文件，
+         * 文件名，完整路径写出到一个单独的txt中保存
+         * FileOutputStream.write("\r\n".getBytes())  写出一个换行符
+         *
+         * 。。。。。。。。。。.png文件，  复制到一个单独的文件夹
          */
-        searchFile(new File("D:\\study\\lesson\\JAVA_stage2\\d029"), "TestDir2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要搜索的文件名或扩展名：");
+        String input = scanner.next();
+        searchFile(new File("D:\\study\\lesson\\JAVA_stage2"), input);
     }
 
     /**
@@ -31,18 +46,21 @@ public class TestFileSearch {
                         if (toSearchName.equals(dir.getName())) {
                             System.out.println("已找到文件:" + dir.getName());
                             System.out.println("地址：" + file.getPath());
-                            //return;
+                            writeFile(file.getName(), file.getPath());
                         }
                         String[] fileLastName = file.getName().split("\\.");
                         if (fileLastName[1].equals(toSearchName)) {
                             System.out.println("找到扩展名为:" + fileLastName[1]);
                             System.out.println("地址：" + file.getPath());
-                            //return;
+                            writeFile(file.getName(), file.getPath());
+                            if(fileLastName[1].equals("png")){
+                                copyFile(file.getPath(),file.getName());
+                            }
                         }
                         if (fileLastName[0].equals(toSearchName)) {
                             System.out.println("找到文件名为:" + fileLastName[0]);
                             System.out.println("地址：" + file.getPath());
-                            //return;
+                            writeFile(file.getName(), file.getPath());
                         }
                     } else if (file.isDirectory()) {
                         //如果是当前file也指向一个目录
@@ -53,4 +71,35 @@ public class TestFileSearch {
         }
     }
 
+    public static void writeFile(String name, String address) {
+
+        try (OutputStream fileOutputStream = new FileOutputStream("D:\\study\\git\\LearnIO\\FileList.txt",true)) {
+            fileOutputStream.write(("文件名：" + name).getBytes(StandardCharsets.UTF_8));
+            fileOutputStream.write(("地址：" + address).getBytes(StandardCharsets.UTF_8));
+            fileOutputStream.write("\r\n".getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyFile(String sourceFile,String fileName){
+        File file = new File(sourceFile);
+
+        try (InputStream fileInputStream = new FileInputStream(file);
+        OutputStream fileOutputStream = new FileOutputStream("D:\\study\\git\\LearnIO\\copyPNG\\"+fileName)){
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, len);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("复制完成");
+    }
 }
