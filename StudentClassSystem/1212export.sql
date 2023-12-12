f010a035aecf75e7d331650d73a815a8087e07c0
Magol <source1990820@163.com>
2023-12-12 15:53:52 +0800

    ER图 与 库导出

---
+++ StudentClassSystem/1212export.sql
@@ -0,0 +1,211 @@
+CREATE DATABASE  IF NOT EXISTS `class_sys_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `class_sys_db`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: class_sys_db
+-- ------------------------------------------------------
+-- Server version	5.7.42-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_id` int(11) NOT NULL COMMENT '班级id',
+  `stu_id` int(11) NOT NULL COMMENT '学生id',
+  `state` int(11) NOT NULL DEFAULT '0' COMMENT '是否出勤，是“1”否“0”迟到“2”',
+  `att_date` date DEFAULT NULL COMMENT '日期时间',
+  `homework` int(3) DEFAULT NULL COMMENT '作业情况，“1”完成，“0”未完成',
+  `comment` varchar(45) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`attendance_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COMMENT='出勤表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendance`
+--
+
+LOCK TABLES `attendance` WRITE;
+/*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
+INSERT INTO `attendance` VALUES (1,1,1,1,'2023-06-10',1,NULL),(2,1,2,1,'2023-06-10',1,NULL),(3,1,3,1,'2023-06-10',1,NULL),(4,1,4,1,'2023-06-10',0,NULL),(5,1,5,0,'2023-06-10',1,NULL),(6,1,1,1,'2023-06-12',1,NULL),(7,1,2,1,'2023-06-12',1,NULL),(8,1,3,1,'2023-06-12',1,NULL),(9,1,4,1,'2023-06-12',1,NULL),(10,1,5,1,'2023-06-12',1,NULL),(11,1,1,0,'2023-06-14',1,NULL),(12,1,2,0,'2023-06-14',1,NULL),(13,1,3,1,'2023-06-14',0,NULL),(14,1,4,1,'2023-06-14',1,NULL),(15,1,5,1,'2023-06-14',1,NULL),(16,1,1,1,'2023-06-16',1,NULL),(17,1,2,1,'2023-06-16',1,NULL),(18,1,3,0,'2023-06-16',0,NULL),(19,1,4,2,'2023-06-16',1,NULL),(20,1,5,2,'2023-06-16',1,NULL);
+/*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `classes` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '班级id',
+  `class_name` varchar(45) NOT NULL COMMENT '班级名称',
+  `cur_id` varchar(45) NOT NULL COMMENT '课程id',
+  `phase_id` int(5) NOT NULL COMMENT '阶段id',
+  `start_time` date NOT NULL COMMENT '班级开始时间',
+  `end_time` date NOT NULL COMMENT '班级结束时间',
+  `teacher_id` int(5) NOT NULL COMMENT '担当教师id',
+  PRIMARY KEY (`class_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='班级表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'java1班','1',1,'2023-06-06','2023-11-06',1),(2,'日语1班','2',3,'2023-03-03','2023-05-03',2),(3,'日语2班','3',6,'2023-05-03','2023-07-03',3);
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `curriculum`
+--
+
+DROP TABLE IF EXISTS `curriculum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `curriculum` (
+  `curriculum_id` int(11) NOT NULL AUTO_INCREMENT,
+  `curriculum_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`curriculum_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='课程表（具体名目）';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curriculum`
+--
+
+LOCK TABLES `curriculum` WRITE;
+/*!40000 ALTER TABLE `curriculum` DISABLE KEYS */;
+INSERT INTO `curriculum` VALUES (1,'java课程'),(2,'日语初级'),(3,'日语口语');
+/*!40000 ALTER TABLE `curriculum` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phase`
+--
+
+DROP TABLE IF EXISTS `phase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `phase` (
+  `phase_id` int(11) NOT NULL AUTO_INCREMENT,
+  `phase_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`phase_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='课程阶段';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phase`
+--
+
+LOCK TABLES `phase` WRITE;
+/*!40000 ALTER TABLE `phase` DISABLE KEYS */;
+INSERT INTO `phase` VALUES (1,'JavaSE'),(2,'JavaWeb'),(3,'日语50音'),(4,'日语基本语法'),(5,'口语语法'),(6,'口语练习');
+/*!40000 ALTER TABLE `phase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stu_class`
+--
+
+DROP TABLE IF EXISTS `stu_class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stu_class` (
+  `stu_class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NOT NULL,
+  `class_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`stu_class_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COMMENT='学生对应班级表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stu_class`
+--
+
+LOCK TABLES `stu_class` WRITE;
+/*!40000 ALTER TABLE `stu_class` DISABLE KEYS */;
+INSERT INTO `stu_class` VALUES (1,1,'1'),(2,2,'1'),(3,3,'1'),(4,4,'1'),(5,5,'1'),(6,3,'2'),(7,5,'2'),(8,7,'2'),(9,9,'2'),(10,6,'3'),(11,8,'3'),(12,2,'3'),(13,4,'3');
+/*!40000 ALTER TABLE `stu_class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `stu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_name` varchar(45) NOT NULL COMMENT '姓名',
+  `stu_start_date` date DEFAULT NULL COMMENT '入学时间',
+  `stu_comment` varchar(45) DEFAULT 'null' COMMENT '备注',
+  PRIMARY KEY (`stu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='学生表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'赵','2023-01-01','null'),(2,'钱','2023-01-02','null'),(3,'孙','2023-01-01','null'),(4,'李','2023-01-03','null'),(5,'周','2023-01-01','null'),(6,'吴','2023-01-05','null'),(7,'郑','2023-01-04','null'),(8,'王','2023-01-09','null'),(9,'冯','2023-01-07','null'),(10,'陈','2023-01-01','null');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacher` (
+  `teacher_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL COMMENT '教师姓名',
+  `comment` varchar(45) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`teacher_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='教师表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` VALUES (1,'张老师',NULL),(2,'王老师',NULL),(3,'李老师',NULL),(4,'赵老师',NULL);
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-12 15:52:28
